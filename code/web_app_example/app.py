0fa648a17ee7cd099166ef2bea0116653ba3cc50
Greg DeLozier <gregdelozier@gmail.com>
2017-09-18 20:17:48 -0400

    Updates for new task

--- code/web_app_example/app.py
+++ code/web_app_example/app.py
@@ -1,23 +1,36 @@
 import sqlite3
-from bottle import route, run, template, debug
+from bottle import route, run, template, debug, request
 
-@route('/todo1')
-def todo1():
-    conn = sqlite3.connect('todo.db')
-    c = conn.cursor()
-    c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
-    result = c.fetchall()
-    return str(result)
-
-@route('/todo')
-def todo():
+@route('/')
+@route('/index.html')
+@route('/tasks')
+def tasks():
     conn = sqlite3.connect('todo.db')
     c = conn.cursor()
     c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
     result = c.fetchall()
     c.close()
-    output = template('make_table', rows=result)
+    output = template('current_tasks', rows=result)
     return output
+
+@route('/new', method='GET')
+def new():
+    return template('new_task')    
+
+@route('/new', method='POST')
+def new_item():
+    new = request.POST.task.strip()
+
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+
+    c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new, 1))
+    new_id = c.lastrowid
+
+    conn.commit()
+    c.close()
+
+    return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
 
 debug(True)
 run(host='localhost', port=8080)
