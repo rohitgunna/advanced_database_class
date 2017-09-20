bfd27d282691fb66f3dba6badb4a308e899e63fc
Greg DeLozier <gregdelozier@gmail.com>
2017-09-20 19:40:27 -0400

    Additional routes for complete and delete.

--- code/web_app_example/app.py
+++ code/web_app_example/app.py
@@ -32,5 +32,29 @@ def new_item():
 
     return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
 
+@route('/complete/<id>', method='GET')
+def complete(id):
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("UPDATE todo SET status = 0 WHERE id LIKE ?", (id,))
+
+    conn.commit()
+    c.close()
+
+    return '<p>The new task was marked complete in the database, the ID is %s</p>' % id
+
+@route('/delete/<id>', method='GET')
+def delete(id):
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("DELETE from todo WHERE id LIKE ?", (id,))
+
+    conn.commit()
+    c.close()
+
+    return '<p>The new task was deleted from the database, the ID is %s</p>' % id
+
+
+
 debug(True)
 run(host='localhost', port=8080)
